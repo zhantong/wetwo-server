775042806e6d41bfe5def56048e50d235e55ec8c
Tong ZHAN <zhantong1994@163.com>
2017-09-11 16:48:25 +0800

    增加登录页面

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import flask_login
 from WeTwo import WeTwo
 
@@ -25,19 +25,10 @@ def user_loader(user_id):
     return user
 
 
-@login_manager.request_loader
-def request_loader(request):
-    user_name = request.form['name']
-    if not user_name or wetwo.is_user_id_exists(wetwo.get_user_id(user_name)):
-        return
-    user = User()
-    user.id = wetwo.get_user_id(user_name)
-    user.is_authenticated = wetwo.is_password_correct(user.id, request.form['password'])
-    return user
-
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        return render_template('login.html')
     user_name = request.form['name']
     if user_name:
         user_id = wetwo.get_user_id(user_name)
@@ -69,4 +60,4 @@ def protected():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)

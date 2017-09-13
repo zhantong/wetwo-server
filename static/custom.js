130fe89efb891b6636b593100b2a4e0f935e1c5d
Tong ZHAN <zhantong1994@163.com>
2017-09-13 21:43:43 +0800

    网页显示用户名而非ID

--- static/custom.js
+++ static/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.btnReplyComment').click(function () {
         var form = $(this).closest('ul').next('form');
         form.find('div.input-group > input[name="parentCommentId"]').val($(this).data('commentid'));
-        form.find('div.input-group > span.input-group-addon').text('回复 ' + $(this).data('userid'));
+        form.find('div.input-group > span.input-group-addon').text('回复 ' + $(this).data('username'));
     });
     $('body').on('submit', '.formComment', function () {
         var formDom = $(this).closest('div.collapse');
